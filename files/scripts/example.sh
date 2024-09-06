62574bc3023064da07904b112cbacd21f5a5feb2
arminmiau <arminmiau.dev@gmail.com>
2024-09-07 01:11:36 +0200

    feat: add and remove more apps

--- files/scripts/example.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-curl -s https://ohmyposh.dev/install.sh | bash -s
-oh-my-posh font install cascadiacode
---
+++ files/scripts/ohmyposh.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+curl -s https://ohmyposh.dev/install.sh | bash -s
+oh-my-posh font install cascadiacode
