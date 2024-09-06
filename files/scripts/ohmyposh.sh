62ef3dc0a0606e83950b0f7803fa9cf420c79e50
arminmiau <arminmiau.dev@gmail.com>
2024-09-07 01:18:51 +0200

    fix: yaml indentation and ohmyposh dir

--- files/scripts/ohmyposh.sh
+++ files/scripts/ohmyposh.sh
@@ -2,5 +2,5 @@
 
 set -oue pipefail
 
-curl -s https://ohmyposh.dev/install.sh | bash -s
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /usr/local/bin
 oh-my-posh font install cascadiacode
